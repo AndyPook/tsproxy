df02f62ae806d86b2f50f27e0ca40640eb00e109
Andy Pook <andy.pook@gmail.com>
2025-12-24 14:19:54 +0000

    debug

--- start.sh
+++ start.sh
@@ -1,17 +1,10 @@
 #!/bin/ash
 #trap 'kill -TERM $PID' TERM INT
 
+env
+
 echo "*** tailscale version"
 tailscale --version
-
-# if [ ! -z "$SKIP_CADDYFILE_GENERATION" ] ; then
-#    echo "Skipping Caddyfile generation as requested via environment"
-# else
-#    echo "Building Caddy configfile"
-
-#    echo $TS_HOSTNAME'.'$TS_TAILNET.'ts.net' > /etc/caddy/Caddyfile
-#    echo 'reverse_proxy' $CADDY_TARGET >> /etc/caddy/Caddyfile
-# fi
 
 echo "*** Starting socat - ${PROXY_TARGET}:${PROXY_PORT}"
 socat -d -d tcp-listen:${PROXY_PORT},fork,reuseaddr tcp:${PROXY_TARGET}:${PROXY_PORT} &
